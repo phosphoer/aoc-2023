2ca5042bb18aa6de54b4427de57a21f06aa9702c
David Evans <david0evans@gmail.com>
2023-12-06 02:04:14 -0800

    puzzle 2 solution and cleanup

--- aoc-2023/Puzzle5.cs
+++ aoc-2023/Puzzle5.cs
@@ -2,22 +2,30 @@ namespace Puzzles
 {
   public static class Puzzle5
   {
+    const bool kOutputSeedInfo = false;
+    const bool kOutputMapsInfo = false;
+    const bool kRunSingleSeedInputs = false;
+    const bool kRunSeedRanges = true;
+
     public static void Run()
     {
+      System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+      stopwatch.Start();
+
       // Parse seed numbers
       string[] inputLines = Puzzle5Input.Input.Split(Environment.NewLine);
       string[] seedStrings = inputLines[0].Split(' ');
-      List<uint> seedInputs = new();
+      List<double> seedInputs = new();
       for (int i = 1; i < seedStrings.Length; ++i)
-        seedInputs.Add(uint.Parse(seedStrings[i]));
+        seedInputs.Add(double.Parse(seedStrings[i]));
 
       List<SeedRange> seedRanges = new();
       for (int i = 1; i + 1 < seedStrings.Length; i += 2)
       {
         seedRanges.Add(new SeedRange()
         {
-          Min = uint.Parse(seedStrings[i]),
-          Length = uint.Parse(seedStrings[i + 1])
+          Min = double.Parse(seedStrings[i]),
+          Length = double.Parse(seedStrings[i + 1])
         });
       }
 
@@ -38,9 +46,9 @@ namespace Puzzles
           string[] valueStrings = line.Split(' ');
           MapRange mapRange = new()
           {
-            DestinationStart = uint.Parse(valueStrings[0]),
-            SourceStart = uint.Parse(valueStrings[1]),
-            Length = uint.Parse(valueStrings[2]),
+            DestinationStart = double.Parse(valueStrings[0]),
+            SourceStart = double.Parse(valueStrings[1]),
+            Length = double.Parse(valueStrings[2]),
           };
 
           currentMap.MapRanges.Add(mapRange);
@@ -48,56 +56,128 @@ namespace Puzzles
       }
 
       // Debug output
-      Console.Write($"Seeds: ");
-      foreach (uint seed in seedInputs)
-        Console.Write($"{seed} ");
-      Console.Write(Environment.NewLine);
-
-      Console.WriteLine($"Seed Ranges: ");
-      uint totalSeedCount = 0;
-      foreach (var seedRange in seedRanges)
+      if (kOutputSeedInfo)
       {
-        totalSeedCount += seedRange.Length;
-        Console.WriteLine($"  {seedRange}");
-      }
-
-      Console.WriteLine($"Seed total: {totalSeedCount}");
-
-      foreach (var seedMap in seedMaps)
-      {
+        Console.Write($"Seeds: ");
+        foreach (double seed in seedInputs)
+          Console.Write($"{seed} ");
         Console.Write(Environment.NewLine);
-        Console.WriteLine(seedMap.Name);
-        foreach (var mapRange in seedMap.MapRanges)
-          Console.WriteLine(mapRange);
-      }
 
-      uint minLocationNumber = uint.MaxValue;
-      List<uint> mappedSeedValues = new();
-      foreach (uint seedValue in seedInputs)
-      {
-        uint mappedValue = seedValue;
-        Console.WriteLine($"Seed {seedValue} maps to:");
-
-        foreach (var seedMap in seedMaps)
+        Console.WriteLine($"Seed Ranges: ");
+        double totalSeedCount = 0;
+        foreach (var seedRange in seedRanges)
         {
-          mappedValue = seedMap.GetMappedValue(mappedValue);
-          Console.WriteLine($"  {seedMap.Name} {mappedValue}");
+          totalSeedCount += seedRange.Length;
+          Console.WriteLine($"  {seedRange}");
         }
 
-        if (mappedValue < minLocationNumber)
-          minLocationNumber = mappedValue;
-
-        mappedSeedValues.Add(mappedValue);
+        Console.WriteLine($"Seed total: {totalSeedCount}");
       }
 
-      Console.WriteLine($"Minimum location number: {minLocationNumber}");
+      if (kOutputMapsInfo)
+      {
+        foreach (var seedMap in seedMaps)
+        {
+          Console.Write(Environment.NewLine);
+          Console.WriteLine(seedMap.Name);
+
+          foreach (var mapRange in seedMap.MapRanges)
+            Console.WriteLine(mapRange);
+        }
+      }
+
+      // Day 1 solution
+      if (kRunSingleSeedInputs)
+      {
+        double minLocationNumber = double.MaxValue;
+        List<double> mappedSeedValues = new();
+        foreach (double seedValue in seedInputs)
+        {
+          double mappedValue = seedValue;
+          Console.WriteLine($"Seed {seedValue} maps to:");
+
+          foreach (var seedMap in seedMaps)
+          {
+            mappedValue = seedMap.GetMappedValue(mappedValue);
+            Console.WriteLine($"  {seedMap.Name} {mappedValue}");
+          }
+
+          if (mappedValue < minLocationNumber)
+            minLocationNumber = mappedValue;
+
+          mappedSeedValues.Add(mappedValue);
+        }
+
+        Console.WriteLine($"Minimum location number: {minLocationNumber}");
+      }
+
+      // Day 2 solution
+      if (kRunSeedRanges)
+      {
+        double currentRange = 0;
+        double minLocationNumber = double.MaxValue;
+        double totalSeedsSkipped = 0;
+        foreach (SeedRange seedRange in seedRanges)
+        {
+          Console.WriteLine($"{currentRange + 1}/{seedRanges.Count}: {seedRange}");
+          minLocationNumber = Math.Min(minLocationNumber, CalculateSeedRangeFast(seedRange, seedMaps, out double skipCount));
+          totalSeedsSkipped += skipCount;
+          currentRange += 1;
+        }
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Minimum location number: {minLocationNumber}");
+        Console.WriteLine($"Total seeds skipped: {totalSeedsSkipped}");
+        Console.WriteLine($"Total duration: {stopwatch.ElapsedMilliseconds}ms");
+      }
+    }
+
+    private static double CalculateSeedRangeFast(SeedRange seedRange, IReadOnlyList<SeedMap> seedMaps, out double totalSkipCount)
+    {
+      totalSkipCount = 0;
+      double minValue = double.MaxValue;
+      for (double seedValue = seedRange.Min; seedValue < seedRange.Max; ++seedValue)
+      {
+        double mappedValue = seedValue;
+
+        // Console.Write($"Seed ");
+
+        double seedSkipAmount = double.MaxValue;
+        foreach (var seedMap in seedMaps)
+        {
+          // Console.Write($"{mappedValue} -> ");
+          mappedValue = seedMap.GetMappedValue(mappedValue, out double skipAmount);
+
+          if (skipAmount < seedSkipAmount)
+            seedSkipAmount = skipAmount;
+
+          // if (skipAmount > 0)
+          //   Console.Write($"(+{skipAmount}) ");
+        }
+
+        // Console.Write($"{mappedValue}");
+        // Console.Write(Environment.NewLine);
+
+        if (seedSkipAmount > 0)
+        {
+          // Console.WriteLine($"Skipping {seedSkipAmount} seeds");
+          seedValue += seedSkipAmount - 1;
+          totalSkipCount += seedSkipAmount;
+        }
+
+        if (mappedValue < minValue)
+          minValue = mappedValue;
+      }
+
+      return minValue;
     }
 
     private struct SeedRange
     {
-      public uint Max => Min + Length - 1;
-      public uint Min;
-      public uint Length;
+      public double Max => Min + Length - 1;
+      public double Min;
+      public double Length;
 
       public override string ToString()
       {
@@ -107,21 +187,24 @@ namespace Puzzles
 
     private struct MapRange
     {
-      public uint SourceStart;
-      public uint DestinationStart;
-      public uint Length;
+      public double SourceEnd => SourceStart + Length - 1;
+      public double DestinationEnd => DestinationStart + Length - 1;
+
+      public double SourceStart;
+      public double DestinationStart;
+      public double Length;
 
       public override string ToString()
       {
         return $"Source: {SourceStart}, Destination: {DestinationStart}, Length: {Length}";
       }
 
-      public bool IsSourceInRange(uint sourceValue)
+      public bool IsSourceInRange(double sourceValue)
       {
-        return sourceValue > SourceStart && sourceValue < SourceStart + Length;
+        return sourceValue >= SourceStart && sourceValue < SourceStart + Length;
       }
 
-      public uint GetMappedValue(uint sourceValue)
+      public double GetMappedValue(double sourceValue)
       {
         return (sourceValue - SourceStart) + DestinationStart;
       }
@@ -132,13 +215,36 @@ namespace Puzzles
       public string Name = string.Empty;
       public List<MapRange> MapRanges = new();
 
-      public uint GetMappedValue(uint sourceValue)
+      public double GetMappedValue(double sourceValue)
       {
         for (int i = 0; i < MapRanges.Count; ++i)
         {
           MapRange range = MapRanges[i];
           if (range.IsSourceInRange(sourceValue))
             return range.GetMappedValue(sourceValue);
+        }
+
+        return sourceValue;
+      }
+
+      public double GetMappedValue(double sourceValue, out double skipAmount)
+      {
+        skipAmount = double.MaxValue;
+        double smallestSourceStart = double.MaxValue;
+        for (int i = 0; i < MapRanges.Count; ++i)
+        {
+          MapRange range = MapRanges[i];
+          if (range.IsSourceInRange(sourceValue))
+          {
+            skipAmount = range.SourceEnd - sourceValue;
+            return range.GetMappedValue(sourceValue);
+          }
+          else if (range.SourceStart > sourceValue && range.SourceStart < smallestSourceStart)
+          {
+            smallestSourceStart = range.SourceStart;
+            double diff = range.SourceStart - sourceValue;
+            skipAmount = diff;
+          }
         }
 
         return sourceValue;
